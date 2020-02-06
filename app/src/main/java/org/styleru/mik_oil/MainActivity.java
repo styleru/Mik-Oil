358c0c47cdd6530a284e0986ae3318578d446004
Vladislav Yundin <4yundin@gmail.com>
2020-02-06 13:26:39 +0300

    Initial commit

---
+++ app/src/main/java/org/styleru/mik_oil/MainActivity.java
@@ -0,0 +1,14 @@
+package org.styleru.mik_oil;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
