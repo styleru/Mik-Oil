67df35663398685d7e396fe034c0cd074383df5c
Vladislav Yundin <4yundin@gmail.com>
2020-02-07 22:04:47 +0300

    Commit messages are important

--- app/src/main/java/org/styleru/mik_oil/MainActivity.java
+++ app/src/main/java/org/styleru/mik_oil/MainActivity.java
@@ -1,8 +1,8 @@
 package org.styleru.mik_oil;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String iDidNotHitHer = "Oh, hi Mark";
     }
 }
