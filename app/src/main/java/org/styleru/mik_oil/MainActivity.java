4ab2adfee1644594bad59c32a5d58e68907563a3
Marina <marina-cheprakova@mail.ru>
2020-02-18 21:32:21 +0300

    Fragments

--- app/src/main/java/org/styleru/mik_oil/MainActivity.java
+++ app/src/main/java/org/styleru/mik_oil/MainActivity.java
@@ -1,14 +1,35 @@
 package org.styleru.mik_oil;
 
+import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+public class MainActivity extends AppCompatActivity implements FragmentEntrance.EntranceFragmentNavigation,
+        FragmentLogin.LoginFragmentNavigation {
 
-public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (getSupportFragmentManager().getBackStackEntryCount() == 0)
+        {
+            goToEntranceFragment();
+        }
+    }
+
+    @Override
+    public void goToLoginFragment(){
+        FragmentLogin fragment = new FragmentLogin();
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.container, fragment)
+                .addToBackStack(null)
+                .commit();
+    }
+
+    public void goToEntranceFragment(){
+        FragmentEntrance fragment = new FragmentEntrance();
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.container, fragment)
+                .commit();
     }
 }
