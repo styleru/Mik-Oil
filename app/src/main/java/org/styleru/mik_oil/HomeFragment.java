6721bbafe11a5ac5071d70a1e1f5f28f8e3f07a5
VyachAndreev <60882539+VyachAndreev@users.noreply.github.com>
2020-02-26 11:29:57 +0300

    Fixing some font issues

--- app/src/main/java/org/styleru/mik_oil/HomeFragment.java
+++ app/src/main/java/org/styleru/mik_oil/HomeFragment.java
@@ -28,16 +28,14 @@ public class HomeFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-        View v = inflater.inflate(R.layout.home_fragment, container, false);
-        ButterKnife.bind(this, v);
-        return v;
+        return inflater.inflate(R.layout.home_fragment, container, false);
+
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
-
+        ButterKnife.bind(this, view);
         enterButton.setOnClickListener(v -> {
             Activity activity = getActivity();
             if (activity instanceof LoginFragment.LoginFragmentNavigator)
