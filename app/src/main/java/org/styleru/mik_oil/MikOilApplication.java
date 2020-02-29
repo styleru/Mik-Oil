52a14b04b02d49a79ca5352f0660dc0ef7abe36c
Vladislav Yundin <4yundin@gmail.com>
2020-02-29 16:10:48 +0300

    Application now provides just context instead of itself. It is better to share most narrow things.

--- app/src/main/java/org/styleru/mik_oil/MikOilApplication.java
+++ app/src/main/java/org/styleru/mik_oil/MikOilApplication.java
@@ -1,13 +1,14 @@
 package org.styleru.mik_oil;
 
 import android.app.Application;
+import android.content.Context;
 
 public class MikOilApplication extends Application {
 
     private static Application INSTANCE;
 
-    public static Application getInstance() {
-        return INSTANCE;
+    public static Context getAppContext() {
+        return INSTANCE.getApplicationContext();
     }
 
     @Override
