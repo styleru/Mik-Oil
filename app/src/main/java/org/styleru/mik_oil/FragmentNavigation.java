d0dc973a233714bc11677991a45eb35774ce51eb
Asideron <45494390+Asideron@users.noreply.github.com>
2020-03-07 22:25:59 +0300

    Individual fragment navigation interfaces were replaced by one for each(FragmentNavigation).

---
+++ app/src/main/java/org/styleru/mik_oil/FragmentNavigation.java
@@ -0,0 +1,9 @@
+package org.styleru.mik_oil;
+
+public interface FragmentNavigation {
+    void goToEntranceFragment();
+
+    void goToLoginFragment();
+
+    void goToRegistrationFragment();
+}
--- app/src/main/java/org/styleru/mik_oil/MainActivity.java
+++ app/src/main/java/org/styleru/mik_oil/MainActivity.java
@@ -4,11 +4,7 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity
-        extends AppCompatActivity
-        implements LoginFragment.LoginFragmentNavigation,
-                   EntranceFragment.EntranceFragmentNavigation,
-                   RegistrationFragment.RegistrationFragmentNavigation {
+public class MainActivity extends AppCompatActivity implements FragmentNavigation {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
