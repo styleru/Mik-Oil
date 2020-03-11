3b31d6afef6f2a83cb5fcc598fb1dd6d709d770d
Savely Prokhorov <reg.savely.prokhorov@gmail.com>
2020-03-11 17:30:50 +0300

    MVP for registration Fragment

--- app/src/main/java/org/styleru/mik_oil/RegistrationFragment.java
+++ app/src/main/java/org/styleru/mik_oil/RegistrationFragment.java
@@ -8,16 +8,19 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.StringRes;
 
 import com.arellomobile.mvp.MvpAppCompatFragment;
+import com.arellomobile.mvp.presenter.InjectPresenter;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
 
-public class RegistrationFragment extends MvpAppCompatFragment {
+public class RegistrationFragment extends MvpAppCompatFragment implements RegistrationView{
     private Unbinder unbinder;
 
     @BindView(R.id.progress_circular)
@@ -33,6 +36,9 @@ public class RegistrationFragment extends MvpAppCompatFragment {
     @BindView(R.id.phone_number)
     TextView phone_number;
 
+    @InjectPresenter
+    RegistrationPresenter presenter;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -43,11 +49,44 @@ public class RegistrationFragment extends MvpAppCompatFragment {
     @Override
     public void onViewCreated(@NonNull View view, Bundle saveInstanceState) {
         ButterKnife.bind(this, view);
+
+        goButton.setOnClickListener(v ->
+                presenter.onRegisterClicked(name.getText().toString(), phone_number.getText().toString(),
+                        password.getText().toString(), password_repeat.getText().toString()));
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         unbinder.unbind();
+    }
+
+    @Override
+    public void setProgressEnabled(boolean enabled) {
+        if (enabled) {
+            goButton.setVisibility(View.INVISIBLE);
+            progressBar.setVisibility(View.VISIBLE);
+        } else {
+            progressBar.setVisibility(View.INVISIBLE);
+            goButton.setVisibility(View.VISIBLE);
+        }
+    }
+
+    @Override
+    public void showToast(String text) {
+        Toast toast = Toast.makeText(getActivity(), text, Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    @Override
+    public void showValidationError(RegistrationView.Field field, @StringRes int messageRes) {
+        String message = getString(messageRes);
+        showToast(message);
+    }
+
+    @Override
+    public void goToMain() {
+        String message = getString(R.string.luck);
+        showToast(message);
     }
 }
