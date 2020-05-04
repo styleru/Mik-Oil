095f9e73a7e9bb48b944dde11e622f5af8ba7525
Amirika18 <stramnova.kate.11@gmail.com>
2020-05-04 12:23:04 +0300

    Added strategy in MainView.

--- app/src/main/java/org/styleru/mik_oil/main/MainView.java
+++ app/src/main/java/org/styleru/mik_oil/main/MainView.java
@@ -1,7 +1,29 @@
 package org.styleru.mik_oil.main;
 
 import com.arellomobile.mvp.MvpView;
+import com.arellomobile.mvp.viewstate.strategy.OneExecutionStateStrategy;
+import com.arellomobile.mvp.viewstate.strategy.StateStrategyType;
 
 import org.styleru.mik_oil.navigation.FragmentNavigator;
 
-interface MainView extends MvpView, FragmentNavigator {}
+interface MainView extends MvpView, FragmentNavigator {
+
+    @StateStrategyType(OneExecutionStateStrategy.class)
+    void goToLoginFragment();
+
+    @StateStrategyType(OneExecutionStateStrategy.class)
+    void goToEntranceFragment();
+
+    @StateStrategyType(OneExecutionStateStrategy.class)
+    void goToPasswordRecoveryFragment();
+
+    @StateStrategyType(OneExecutionStateStrategy.class)
+    void goToVerificationKeyFragment();
+
+    @StateStrategyType(OneExecutionStateStrategy.class)
+    void goToRegistrationFragment();
+
+    @StateStrategyType(OneExecutionStateStrategy.class)
+    void goToProfileFragment();
+
+}
