5797a5d8cff6df9e564c4af3d4a728b9c612f48c
Vladislav Yundin <4yundin@gmail.com>
2020-05-05 19:37:53 +0300

    MainView changes

--- app/src/main/java/org/styleru/mik_oil/main/MainView.java
+++ app/src/main/java/org/styleru/mik_oil/main/MainView.java
@@ -4,24 +4,10 @@ import com.arellomobile.mvp.MvpView;
 import com.arellomobile.mvp.viewstate.strategy.OneExecutionStateStrategy;
 import com.arellomobile.mvp.viewstate.strategy.StateStrategyType;
 
-import org.styleru.mik_oil.navigation.FragmentNavigator;
-
-interface MainView extends MvpView, FragmentNavigator {
-
-    @StateStrategyType(OneExecutionStateStrategy.class)
-    void goToLoginFragment();
+interface MainView extends MvpView {
 
     @StateStrategyType(OneExecutionStateStrategy.class)
     void goToEntranceFragment();
-
-    @StateStrategyType(OneExecutionStateStrategy.class)
-    void goToPasswordRecoveryFragment();
-
-    @StateStrategyType(OneExecutionStateStrategy.class)
-    void goToVerificationKeyFragment();
-
-    @StateStrategyType(OneExecutionStateStrategy.class)
-    void goToRegistrationFragment();
 
     @StateStrategyType(OneExecutionStateStrategy.class)
     void goToProfileFragment();
